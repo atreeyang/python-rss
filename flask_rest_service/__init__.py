8d8f729a418ef6aba100f9a912ffe6f6d0c55098
xingshi <chengyang.yan@outlook.com>
2015-09-16 00:55:14 +0800

    test

--- flask_rest_service/__init__.py
+++ flask_rest_service/__init__.py
@@ -1,9 +1,13 @@
-import os
+# coding: utf-8
+import threading
+import time, os, sched
 from flask import Flask
 from flask.ext import restful
 from flask.ext.pymongo import PyMongo
 from flask import make_response
 from bson.json_util import dumps
+from pymongo import MongoClient
+import feedparser
 
 MONGO_URL = os.environ.get('MONGO_URL')
 if not MONGO_URL:
@@ -22,5 +26,41 @@ def output_json(obj, code, headers=None):
 DEFAULT_REPRESENTATIONS = {'application/json': output_json}
 api = restful.Api(app)
 api.representations = DEFAULT_REPRESENTATIONS
+
+
+urls = ['http://rss.dailyfx.com.hk/cmarkets_chg_sc.xml',
+        'http://rss.dailyfx.com.hk/commentary_morning_chg_sc.xml']
+
+client = MongoClient(MONGO_URL)
+
+def readRss(urls):
+    posts = client.rss.readings
+    print("refresh the news rss==")
+    for url in urls:
+        items = feedparser.parse(url)
+        for entry in items.entries:
+            if (posts.find_one({"link":entry.link})):
+                continue
+
+            str_pubDate = strftime("%Y-%m-%d %H:%M:%S",entry.date_parsed)
+            d = pq(url=entry.link)
+            content = d(".content").html()
+
+            post={"title":entry.title, "link":entry.link,
+                  "published":str_pubDate,
+                  "date": datetime.fromtimestamp(mktime(entry.published_parsed)),
+                  "summary":entry.summary,
+                  'content':content}
+            post_id = posts.insert(post)
+            print post_id
+    posts.create_index([("date", -1)])
+    posts.create_index([("link", 1)])
+
+def refreshRss():
+    readRss(urls)
+    print(time.ctime())
+    threading.Timer(60*5, refreshRss).start()
+
+refreshRss();
 
 import flask_rest_service.resources
--- rss.py
+++ rss.py
@@ -7,39 +7,54 @@ from pyquery import PyQuery as pq
 from time import mktime, strftime
 from datetime import datetime
 
-urls = ['http://rss.dailyfx.com.hk/cmarkets_chg_sc.xml',
-        'http://rss.dailyfx.com.hk/commentary_morning_chg_sc.xml'];
-
 client = MongoClient('localhost', 27017)
 db = client.rss
 posts = db.readings
-posts.drop()
+#posts.drop()
+
+#===========================================
+urls = ['http://rss.dailyfx.com.hk/cmarkets_chg_sc.xml',
+        'http://rss.dailyfx.com.hk/commentary_morning_chg_sc.xml']
 
 def readRss(urls):
-    i = 0
+    print("refresh the news rss==")
+    posts = mongo.db.readings
     for url in urls:
         items = feedparser.parse(url)
         for entry in items.entries:
+            if (posts.find_one({"link":entry.link})):
+                continue
+
+            str_pubDate = strftime("%Y-%m-%d %H:%M:%S",entry.date_parsed)
             d = pq(url=entry.link)
             content = d(".content").html()
-            str_pubDate = strftime("%Y-%m-%d %H:%M:%S",entry.date_parsed)
-            print(str_pubDate)
+
             post={"title":entry.title, "link":entry.link,
                   "published":str_pubDate,
                   "date": datetime.fromtimestamp(mktime(entry.published_parsed)),
                   "summary":entry.summary,
                   'content':content}
             post_id = posts.insert(post)
-
     posts.create_index([("date", -1)])
+    posts.create_index([("link", 1)])
 
-def getHtml(url):
-    page = urllib.urlopen(url)
-    html = page.read()
-    return html
+schedule = sched.scheduler(time.time, time.sleep)
+
+def perform_command(cmd, inc):
+    schedule.enter(inc, 0, perform_command, (cmd, inc))
+    os.system(cmd)
+    readRss(urls)
+
+def timming_exe(cmd, inc = 60*10):
+    schedule.enter(inc, 0, perform_command, (cmd, inc))
+    schedule.run()
+
+timming_exe("echo %time%")
+#=====================================
+
 
 readRss(urls)
-print posts.find_one()
+
 
 
 #print getHtml("http://www.dailyfx.com.hk/commentary/morning-20150731-2779.html")
