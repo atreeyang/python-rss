8e3703ddb6d237bd6a9f993cfc38caa7b26a56b8
atreeyang <chengyang.yan@outlook.com>
2015-09-21 01:02:26 +0800

    Update resources.py

--- flask_rest_service/resources.py
+++ flask_rest_service/resources.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import json
 from flask import request, abort
 from flask.ext import restful
@@ -25,7 +27,8 @@ class ReadingList(restful.Resource):
         print(offset)
         print(limit)
         res = {'status':0,
-               'entries':[x for x in mongo.db.readings.find().sort('date',-1).skip(offset).limit(limit)]}
+               'entries':[x for x in mongo.db.readings.find({},{'title':1,'published':1,'subcat':1})
+                          .sort('date',-1).skip(offset).limit(limit)]}
         return  res
 
 
@@ -57,8 +60,26 @@ def entry_detail(id):
     return render_template('entry.html', entry=entry)
 
 
-urls = ['http://rss.dailyfx.com.hk/cmarkets_chg_sc.xml',
-        'http://rss.dailyfx.com.hk/commentary_morning_chg_sc.xml']
+urls = [{'cat':'dailfx', 'subcat':'市场回音', 'url':'http://rss.dailyfx.com.hk/cmarkets_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'纽约盘后', 'url':'http://rss.dailyfx.com.hk/commentary_morning_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'欧洲盘前', 'url':'http://rss.dailyfx.com.hk/commentary_afternoon_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'纽约盘前', 'url':'http://rss.dailyfx.com.hk/commentary_evening_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'美元指数', 'url':'http://rss.dailyfx.com.hk/us_dollar_index_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'美元指数技术分析', 'url':'http://rss.dailyfx.com.hk/us_dollar_index_techs_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'外汇名家', 'url':'http://rss.dailyfx.com.hk/analysts_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'技术分析', 'url':'http://rss.dailyfx.com.hk/techs_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'论交叉盘', 'url':'http://rss.dailyfx.com.hk/crosses_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'直盘分析', 'url':'http://rss.dailyfx.com.hk/charts_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'今日看盘', 'url':'http://rss.dailyfx.com.hk/intraday_techs_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'牛熊榜', 'url':'http://rss.dailyfx.com.hk/winners_and_losers_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'专题探讨', 'url':'http://rss.dailyfx.com.hk/feaarticle_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'期货变化', 'url':'http://rss.dailyfx.com.hk/cotreport_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'投机情绪指数', 'url':'http://rss.dailyfx.com.hk/fxcmssi_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'每周基本分析展望', 'url':'http://rss.dailyfx.com.hk/outlook_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'每周交易策略', 'url':'http://rss.dailyfx.com.hk/weekly_strategy_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'金属产品', 'url':'http://rss.dailyfx.com.hk/metal_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'股市原油', 'url':'http://rss.dailyfx.com.hk/stocks_oil_chg_sc.xml'},
+        {'cat':'dailfx', 'subcat':'机构报告', 'url':'http://rss.dailyfx.com.hk/institution_chg_sc.xml'}]
 
 
 client = MongoClient(app.config['MONGO_URI'])
@@ -67,23 +88,27 @@ def readRss(urls):
     posts = client.get_default_database().readings
     print("refresh the news rss==")
     for url in urls:
-        items = feedparser.parse(url)
+        items = feedparser.parse(url['url'])
         for entry in items.entries:
             if (posts.find_one({"link":entry.link})):
                 continue
 
-            str_pubDate = strftime("%Y-%m-%d %H:%M:%S",entry.date_parsed)
+            str_pubDate = strftime("%Y-%m-%d %H:%M",entry.date_parsed)
             d = pq(url=entry.link)
             content = d(".content").html()
 
             post={"title":entry.title, "link":entry.link,
                   "published":str_pubDate,
                   "date": datetime.fromtimestamp(mktime(entry.published_parsed)),
-                  "summary":entry.summary,
+                  #"summary":entry.summary,
+                  'cat':url['cat'],
+                  'subcat':url['subcat'],
                   'content':content}
             post_id = posts.insert(post)
             print(post_id)
+
     posts.create_index([("date", -1)])
+    posts.create_index([("cat", -1)])
     posts.create_index([("link", 1)])
 
 def refreshRss():
@@ -99,6 +124,11 @@ def init():
         refreshRss()
         app.config['inited'] = 1
     return "init...."
+
+@app.route('/clear')
+def clear():
+    client.get_default_database().readings.drop()
+    return "clear...."
 
 
 api.add_resource(ReadingList, '/readings/')
